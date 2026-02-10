0eeb0bfc62b3d357fd4cca90e52484ad20740f8b
laumaq <wod58@hotmail.com>
2026-02-10 14:54:56 +0100

    Update title in layout to 'Portail Waha - Outils Scolaires'

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Portail TFH - Outils Scolaires',
+  title: 'Portail Waha - Outils Scolaires',
   description: 'Portail des outils pédagogiques',
 };
 
