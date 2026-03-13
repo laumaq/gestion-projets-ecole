3f470e7776f9964cec8f036dc80d8a3e648bd2c7
laumaq <wod58@hotmail.com>
2026-03-13 09:15:29 +0100

    mise en place module expériences collaboratives et simulation de circuits

--- next.config.js
+++ next.config.js
@@ -4,6 +4,19 @@ const nextConfig = {
   images: {
     domains: [], // Ajoutez vos domaines d'images si nécessaire
   },
+  webpack: (config, { isServer }) => {
+    // Résoudre le problème avec konva (uniquement côté serveur)
+    if (isServer) {
+      config.externals.push('canvas');
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false,
+        fs: false,
+        path: false,
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
